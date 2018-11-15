c2be312c89f6c93bcc80f2198d5947c7a7835bb4
playunits <32098383+playunits@users.noreply.github.com>
2018-11-15 23:57:55 +0100

    Removed some testing Stuff

--- playlib/lua/playlib/functions/vgui.lua
+++ playlib/lua/playlib/functions/vgui.lua
@@ -3,40 +3,9 @@ if !PLAYLIB then return end
 PLAYLIB.vgui = PLAYLIB.vgui or {}
 
 if SERVER then -- Serverside Code here
-    util.AddNetworkString("PLAYLIB::Test")
 
-	hook.Add("PlayerSay","PLAYLIB::TEST",function(ply,text) 
-		if text == "!test" then
-			net.Start("PLAYLIB::Test")
-			net.Send(ply)
-		end
-	end)
 elseif CLIENT then -- Clientside Code here
-    net.Receive("PLAYLIB::Test",function()
-    /* 
-    	PLAYLIB.vgui.valueDerma("Workshop Content","Workshop ID",
-            "Abgeben",function(self,val) LocalPlayer():ChatPrint(val) self:GetParent():Close() end,
-            "Abbrechen",function(self) LocalPlayer():ChatPrint("Nein")  self:GetParent():Close() end,
-            true)
-    */
-        local val = vgui.Create("play_derma_value")
-        val:Setup({
-            ["title"] = "Trag einen Wert ein!",
-            ["sText"] = "Abgeben!",
-            ["sFunc"] = function(self,val)
-                LocalPlayer():ChatPrint(val)
-            end,
-            ["aFunc"] = function(self)
-                LocalPlayer():ChatPrint("Abgebrochen")
-            end,
-            ["aText"] = "Nein!",
-            ["placeholder"] = "SID64",
-            ["blur"] = true
-        })
-        --PLAYLIB.vgui.valueDerma(ltitle,ltext,placeholder,sName,sFunc,aName,aFunc,blur)
-    end)
-
-	/*
+    /*
 		Simple Creation of Yes/No Window
         
         Must Have Params
@@ -49,7 +18,35 @@ elseif CLIENT then -- Clientside Code here
 
 	*/
 
-    function PLAYLIB.vgui.ynDerma(title,ltext,yName,yFunc,nName,nFunc,blur)
+    function PLAYLIB.vgui.ynDerma(title,question,yText,yFunc,nName,nFunc,blur)
+
+        local yn = vgui.Create("play_derma_yn")
+        yn:Setup({
+            ["title"] = title,
+            ["yText"] = yText,
+            ["yFunc"] = yFunc,
+            ["nFunc"] = nFunc,
+            ["nText"] = nName,
+            ["question"] = question,
+            ["blur"] = blur
+        })
+    end
+
+
+    /*
+        Simple Creation of Value Window
+        
+        Must Have Params
+        @Params title (String) = The Window Title
+        @Params text (String) = The Text displayed in the Window
+        @Params yName (String) = The Name of the Y Button
+        @Params yFunc (Function) = The Function to be executed on Y Button Press
+        @Params nName (String) = The Name of the N Button
+        @Params nFunc (Function) = The Function to be executed on N Button Press
+
+    */
+
+    function PLAYLIB.vgui.valueDerma(title,placeholder,sText,sFunc,aText,aFunc,blur)
 
         local val = vgui.Create("play_derma_value")
         val:Setup({
@@ -61,21 +58,6 @@ elseif CLIENT then -- Clientside Code here
             ["placeholder"] = placeholder,
             ["blur"] = blur
         })
-
-    end
-
-        function PLAYLIB.vgui.valueDerma(title,question,yText,yFunc,nName,nFunc,blur)
-
-            local yn = vgui.Create("play_derma_yn")
-            yn:Setup({
-                ["title"] = title,
-                ["yText"] = yText,
-                ["yFunc"] = yFunc,
-                ["nFunc"] = nFunc,
-                ["nText"] = nName,
-                ["question"] = question,
-                ["blur"] = blur
-            })
     end
 
     function PLAYLIB.vgui.blur( panel, amount, times )
