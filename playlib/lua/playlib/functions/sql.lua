03d4b7499bc07c8ba0654efca732de38f133b242
playunits <32098383+playunits@users.noreply.github.com>
2018-11-15 06:26:29 +0100

    Added sql.lua Functions

---
+++ playlib/lua/playlib/functions/sql.lua
@@ -0,0 +1,32 @@
+if !PLAYLIB then return end
+
+PLAYLIB.sql = PLAYLIB.sql or {}
+
+if SERVER then -- Serverside Code here
+    
+elseif CLIENT then -- Clientside Code here
+    
+end
+
+function PLAYLIB.sql.query(sql,query, func)
+	if not sql then
+		MySQLite.query(query, function(res)
+			if func then
+				func(res)
+			end
+		end)
+	else
+		local res = sql.Query(query)
+		if func then
+			func(res)
+		end
+	end
+end
+
+function PLAYLIB.sql.sqlStr(sql,str)
+	if not sql then
+		return MySQLite.SQLStr(str)
+	else
+		return sql.SQLStr(str)
+	end
+end
